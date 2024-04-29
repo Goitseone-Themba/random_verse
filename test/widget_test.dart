9e870423679acc96ae94b45c59c673726e98083e
Goitseone-Themba <goitseonerozthemba@gmail.com>
2024-04-29 12:55:08 +0200

    changed globe to random-verse

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:globe/main.dart';
+import 'package:random_verse/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
