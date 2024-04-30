9d2b10023ac9f6a7a3454e0d89539fe6e512fb51
Goitseone_Themba <97857682+Goitseone-Themba@users.noreply.github.com>
2024-04-30 14:51:57 +0200

    working, but not validated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'monte_carlo.dart';
@@ -30,29 +31,35 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   List<int> lengths = [];
 
-  startSimulation() {
-    lengths = monteCarloSimulation(100, 10000);
+  startSimulation(int n, int N) {
+    lengths = monteCarloSimulation(n, N);
     notifyListeners();
   }
 }
 
+//My Data model for the histogram
 class MaxCycleLength {
   final double yValue;
   MaxCycleLength(this.yValue);
 }
 
+//Has an appbar and a column with 4 rows
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var lengthData = appState.lengths;
+    int expectedMax = lengthData.reduce(max);
 
     final List<MaxCycleLength> chartData =
         lengthData.map((e) => MaxCycleLength(e.toDouble())).toList();
 
+    final arraySizeController = TextEditingController();
+    final permController = TextEditingController();
+
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
+          // backgroundColor: Colors.white,
           automaticallyImplyLeading: false,
           leading: Icon(
             Icons.language_rounded,
@@ -68,40 +75,124 @@ class MyHomePage extends StatelessWidget {
         ),
         body: Center(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text('Enter n : '),
+                  SizedBox(
+                    width: 360,
+                    child: TextField(
+                      controller: arraySizeController,
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                        hintText: 'Enter array size',
+                        border: OutlineInputBorder(),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            arraySizeController.clear();
+                          },
+                          icon: Icon(Icons.clear),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 360,
+                    child: TextField(
+                      controller: permController,
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                        hintText: 'Enter number of permutations:',
+                        border: OutlineInputBorder(),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            permController.clear();
+                          },
+                          icon: Icon(Icons.clear),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    appState.startSimulation();
-                  },
-                  child: Text('Generate')),
-              SizedBox(
-                height: 128,
-                child: Text('The length is $lengthData'),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        appState.startSimulation(int.parse(arraySizeController.text), int.parse(permController.text));
+                      },
+                      icon: Icon(
+                        Icons.directions_run_outlined,
+                        size: 24,
+                      ),
+                      label: Text('Generate')),
+                ],
               ),
-              SizedBox(
-                height: 480,
-                width: 720,
-                child: chartData.isNotEmpty
-                    ? SfCartesianChart(
-                        series: <CartesianSeries>[
-                          HistogramSeries<MaxCycleLength, num>(
-                            dataSource: chartData,
-                            yValueMapper: (MaxCycleLength cycleLength, _) =>
-                                cycleLength.yValue,
-                            binInterval: 5,
-                            showNormalDistributionCurve: true,
-                            curveColor: Colors.deepOrange,
-                            borderWidth: 3,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Distribution of Maximum Cycle Length",
+                      style: TextStyle(fontSize: 24)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 720,
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 4,
+                                color: Color(0x33000000),
+                                offset: Offset(
+                                  -12,
+                                  12,
+                                ),
+                              )
+                            ],
                           ),
-                        ],
-                      )
-                    : SizedBox(),
+                          child: SizedBox(
+                            height: 480,
+                            width: 720,
+                            child: chartData.isNotEmpty
+                                ? SfCartesianChart(
+                                    series: <CartesianSeries>[
+                                      HistogramSeries<MaxCycleLength, num>(
+                                        dataSource: chartData,
+                                        yValueMapper:
+                                            (MaxCycleLength cycleLength, _) =>
+                                                cycleLength.yValue,
+                                        binInterval: 5,
+                                        showNormalDistributionCurve: true,
+                                        curveColor: Colors.deepOrange,
+                                        borderWidth: 3,
+                                      ),
+                                    ],
+                                  )
+                                : SizedBox(),
+                          ),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text('Expected Maximum Cycle Length: $expectedMax'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
